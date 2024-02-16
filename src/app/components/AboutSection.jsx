235d87105eb6071101a2d17a4f1af5c77b59bc46
knyhtlikeb <159398362+knyhtlikeb@users.noreply.github.com>
2024-02-16 07:31:43 +0700

    message

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,104 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Junior HTML,CSS & JavaScript</li>
+        <li>Junior Express.js</li>
+        <li>Junior Node.js</li>
+        <li>Junior React</li>
+        <li>Junior Tailwind</li>
+        <li>Customer Service & Support</li>
+        <li>Community Management</li>
+        <li>Creative Content Creator</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>X1</li>
+        <li>X2</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>X1</li>
+        <li>X2</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image 
+        src="/images/about-image.png"
+        alt="about-image.png" 
+        width={500} 
+        height={500} 
+        />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+          I am a dedicated and results-driven professional known for reliability, adaptability, and thriving under pressure. Excels in collaborative team environments, taking proactive initiatives to ensure exceptional service and meet deadlines.
+          Self-taught in software development, currently mastering the MERN stack with a main focus on NextJS. Passionate about cryptocurrency, I developed a Solana blockchain token, demonstrating proficiency in this dynamic field. Adept at navigating developer environments and committed to continuous improvement. Bringing a customer-centric approach
+          and a flair for cutting-edge technologies in dynamic work environments.
+
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
